28355df8c0b060c5f03beb6aecfc6850a7adcdd0
TOPSTAR <drnn@bk.ru>
2016-09-07 21:21:39 +0300

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -25,10 +25,7 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        long start = System.currentTimeMillis();
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-        long finish = System.currentTimeMillis();
-        System.out.println(finish - start);
 //        .toLocalDate();
 //        .toLocalTime();
     }
