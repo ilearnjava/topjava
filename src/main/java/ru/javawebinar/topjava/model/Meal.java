14b0e2fab32aebbd8546e45ba0e75ceee6325c2f
TOPSTAR <drnn@bk.ru>
2016-09-20 14:14:27 +0300

    HW1

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * GKislin
@@ -15,10 +16,14 @@ public class Meal {
 
     private final int calories;
 
+    private AtomicInteger id = new AtomicInteger(0);
+
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.id.incrementAndGet();
+        System.out.println(id);
     }
 
     public LocalDateTime getDateTime() {
@@ -39,5 +44,9 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public AtomicInteger getId() {
+        return id;
     }
 }
--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * GKislin
@@ -15,11 +16,15 @@ public class MealWithExceed {
 
     private final boolean exceed;
 
-    public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    private AtomicInteger id;
+
+
+    public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed, AtomicInteger id) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.exceed = exceed;
+        this.id = id;
     }
 
     @Override
@@ -30,5 +35,25 @@ public class MealWithExceed {
                 ", calories=" + calories +
                 ", exceed=" + exceed +
                 '}';
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean getExceed() {
+        return exceed;
+    }
+
+    public AtomicInteger getId() {
+        return id;
     }
 }
